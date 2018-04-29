0a05197770f527d17cd465424779ac93bb509f8b
Joren Van Hee <jorenvanhee@gmail.com>
2018-04-29 14:46:04 +0200

    styles wip

--- tailwind.js
+++ tailwind.js
@@ -46,87 +46,15 @@ View the full documentation at https://tailwindcss.com.
 let colors = {
   'transparent': 'transparent',
 
-  'black': '#22292f',
-  'grey-darkest': '#3d4852',
-  'grey-darker': '#606f7b',
-  'grey-dark': '#8795a1',
-  'grey': '#b8c2cc',
-  'grey-light': '#dae1e7',
-  'grey-lighter': '#f1f5f8',
-  'grey-lightest': '#f8fafc',
-  'white': '#ffffff',
-
-  'red-darkest': '#3b0d0c',
-  'red-darker': '#621b18',
-  'red-dark': '#cc1f1a',
-  'red': '#e3342f',
-  'red-light': '#ef5753',
-  'red-lighter': '#f9acaa',
-  'red-lightest': '#fcebea',
-
-  'orange-darkest': '#462a16',
-  'orange-darker': '#613b1f',
-  'orange-dark': '#de751f',
-  'orange': '#f6993f',
-  'orange-light': '#faad63',
-  'orange-lighter': '#fcd9b6',
-  'orange-lightest': '#fff5eb',
-
-  'yellow-darkest': '#453411',
-  'yellow-darker': '#684f1d',
-  'yellow-dark': '#f2d024',
-  'yellow': '#ffed4a',
-  'yellow-light': '#fff382',
-  'yellow-lighter': '#fff9c2',
-  'yellow-lightest': '#fcfbeb',
-
-  'green-darkest': '#0f2f21',
-  'green-darker': '#1a4731',
-  'green-dark': '#1f9d55',
-  'green': '#38c172',
-  'green-light': '#51d88a',
-  'green-lighter': '#a2f5bf',
-  'green-lightest': '#e3fcec',
-
-  'teal-darkest': '#0d3331',
-  'teal-darker': '#20504f',
-  'teal-dark': '#38a89d',
-  'teal': '#4dc0b5',
-  'teal-light': '#64d5ca',
-  'teal-lighter': '#a0f0ed',
-  'teal-lightest': '#e8fffe',
-
-  'blue-darkest': '#12283a',
-  'blue-darker': '#1c3d5a',
-  'blue-dark': '#2779bd',
-  'blue': '#3490dc',
-  'blue-light': '#6cb2eb',
-  'blue-lighter': '#bcdefa',
-  'blue-lightest': '#eff8ff',
-
-  'indigo-darkest': '#191e38',
-  'indigo-darker': '#2f365f',
-  'indigo-dark': '#5661b3',
-  'indigo': '#6574cd',
-  'indigo-light': '#7886d7',
-  'indigo-lighter': '#b2b7ff',
-  'indigo-lightest': '#e6e8ff',
-
-  'purple-darkest': '#21183c',
-  'purple-darker': '#382b5f',
-  'purple-dark': '#794acf',
-  'purple': '#9561e2',
-  'purple-light': '#a779e9',
-  'purple-lighter': '#d6bbfc',
-  'purple-lightest': '#f3ebff',
-
-  'pink-darkest': '#451225',
-  'pink-darker': '#6f213f',
-  'pink-dark': '#eb5286',
-  'pink': '#f66d9b',
-  'pink-light': '#fa7ea8',
-  'pink-lighter': '#ffbbca',
-  'pink-lightest': '#ffebef',
+  'black': '#1d1715',
+  'grey-darkest': '#403732',
+  // 'grey-darker': '#606f7b',
+  // 'grey-dark': '#8795a1',
+  // 'grey': '#b8c2cc',
+  'grey-light': '#d8d1ce',
+  // 'grey-lighter': '#f1f5f8',
+  'grey-lightest': '#f9f7f6',
+  'white': '#fcfbfb',
 }
 
 module.exports = {
@@ -194,20 +122,13 @@ module.exports = {
 
   fonts: {
     'sans': [
-      'system-ui',
-      'BlinkMacSystemFont',
-      '-apple-system',
-      'Segoe UI',
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      'Fira Sans',
+      'Assistant',
       'Droid Sans',
       'Helvetica Neue',
       'sans-serif',
     ],
     'serif': [
+      'Lora',
       'Constantia',
       'Lucida Bright',
       'Lucidabright',
@@ -220,11 +141,11 @@ module.exports = {
       'serif',
     ],
     'mono': [
-      'Menlo',
-      'Monaco',
-      'Consolas',
-      'Liberation Mono',
-      'Courier New',
+      // 'Menlo',
+      // 'Monaco',
+      // 'Consolas',
+      // 'Liberation Mono',
+      // 'Courier New',
       'monospace',
     ]
   },
@@ -250,15 +171,18 @@ module.exports = {
   */
 
   textSizes: {
-    'xs': '.75rem',     // 12px
-    'sm': '.875rem',    // 14px
-    'base': '1rem',     // 16px
-    'lg': '1.125rem',   // 18px
-    'xl': '1.25rem',    // 20px
-    '2xl': '1.5rem',    // 24px
-    '3xl': '1.875rem',  // 30px
-    '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    // 'xs': '.75rem',     // 12px
+    // 'sm': '.875rem',    // 14px
+    'base': '1rem',     // 18px
+    'lg': '1.1111111rem',     // 20px
+    'xl': '1.3333333rem',     // 24px
+    '2xl': '2rem',     // 36px
+    // 'lg': '1.125rem',   // 18px
+    // 'xl': '1.25rem',    // 20px
+    // '2xl': '1.5rem',    // 24px
+    // '3xl': '1.875rem',  // 30px
+    // '4xl': '2.25rem',   // 36px
+    // '5xl': '3rem',      // 48px
   },
 
 
@@ -303,8 +227,8 @@ module.exports = {
 
   leading: {
     'none': 1,
-    'tight': 1.25,
-    'normal': 1.5,
+    'tight': 1.3333333,
+    'normal': 1.6111111,
     'loose': 2,
   },
 
@@ -439,11 +363,11 @@ module.exports = {
   */
 
   borderRadius: {
-    'none': '0',
-    'sm': '.125rem',
+    // 'none': '0',
+    'sm': '3px',
     default: '.25rem',
-    'lg': '.5rem',
-    'full': '9999px',
+    // 'lg': '.5rem',
+    // 'full': '9999px',
   },
 
 
@@ -675,8 +599,10 @@ module.exports = {
     '2': '0.5rem',
     '3': '0.75rem',
     '4': '1rem',
-    '6': '1.5rem',
+    '6': '1.4444444rem',
     '8': '2rem',
+    '9': '2.5rem',
+    '10': '3rem',
   },
 
 
@@ -725,6 +651,7 @@ module.exports = {
 
   shadows: {
     default: '0 2px 4px 0 rgba(0,0,0,0.10)',
+    'sm': '0 2px 3px rgba(207, 199, 195, 0.16)',
     'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
     'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
     'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
@@ -899,10 +826,10 @@ module.exports = {
   */
 
   plugins: [
-    require('tailwindcss/plugins/container')({
+    // require('tailwindcss/plugins/container')({
       // center: true,
       // padding: '1rem',
-    }),
+    // }),
   ],
 
 
