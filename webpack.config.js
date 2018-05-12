76ca0cfb4cbab4d5ba5cbe47553ba9694eeb0550
Joren Van Hee <jorenvanhee@gmail.com>
2018-05-12 22:48:48 +0200

    Add tailwind purgecss extractor

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ var PurgecssPlugin = require('purgecss-webpack-plugin')
 var glob = require('glob')
 var path = require('path')
 
+/**
+ * Custom PurgeCSS extractor for Tailwind that allows special characters in
+ * class names.
+ *
+ * https://github.com/FullHuman/purgecss#extractor
+ */
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
 module.exports = function (env, argv) {
   var isProduction = argv.mode === 'production';
 
@@ -40,7 +52,13 @@ module.exports = function (env, argv) {
         filename: 'app.css'
       }),
       new PurgecssPlugin({
-        paths: glob.sync('_site/**/*.html')
+        paths: glob.sync('_site/**/*.html'),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ['html', 'js', 'php', 'vue']
+          }
+        ]
       })
     ]
   }
